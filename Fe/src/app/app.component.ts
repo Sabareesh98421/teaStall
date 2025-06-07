7c2d996942ae1024b65e52205b40c75fc6ffd3fc
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-06-07 23:41:59 +0530

    Refactor: use inject, cleanup logic

--- Fe/src/app/app.component.ts
+++ Fe/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecordAudioService } from '../service/record-audio.service';
-
+import { HttpHandlerService } from '@core/http-handler.service';
+import { environment } from '@env/environment.development';
+// import { RecordAudioService } from '@service/record-audio.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,13 +11,20 @@ import { RecordAudioService } from '../service/record-audio.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'teaSpot';
+  private readonly apiUrl = environment.apiUrl;
   private recordAudio = inject(RecordAudioService)
-  constructor() {
-
-    this.startRecording();
-
+  private http = inject(HttpHandlerService);
+  protected serverStatusMessage: string = "";
+  //  I don't need of constructor here because I am using the inject function to get the RecordAudioService instance
+  ngOnInit(): void {
+    this.http.get<{ welcome: string }>(this.apiUrl).subscribe((res) => {
+      console.log(res.welcome);
+      this.serverStatusMessage = res.welcome;
+    })
+    // this.startRecording();
   }
   async startRecording() {
     try {
